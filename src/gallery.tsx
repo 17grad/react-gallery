6724f24fc6e1ed0f6c85bd5d5f87cbbb5849bfa2
Dean Jürges <dean@17grad.com>
2019-09-03 14:05:25 +0200

    Initial commit

---
+++ src/gallery.tsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import Swiper from 'react-id-swiper';
+
+import * as styles from './gallery.scss';
+import { Image } from 'core/models/image';
+
+interface GalleryProps {
+  thumbnails: Image[];
+  images: Image[];
+}
+
+export const Gallery = (props: GalleryProps) => {
+  const [gallerySwiper, getGallerySwiper]: [any, any] = useState(null);
+  const [thumbnailSwiper, getThumbnailSwiper]: [any, any] = useState(null);
+  const [isLightboxOpen, setIsLightboxOpen]: [boolean, any] = useState(false);
+
+  const gallerySwiperParams = {
+    getSwiper: getGallerySwiper,
+    navigation: {
+      nextEl: '.lightbox__button-next',
+      prevEl: '.lightbox__button-prev',
+    },
+    preloadImages: true,
+    lazy: true
+  };
+  const thumbnailSwiperParams = {
+    getSwiper: getThumbnailSwiper,
+    spaceBetween: 10,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    touchRatio: 0.2,
+    slideToClickedSlide: true
+  };
+
+  useEffect(() => {
+    if (gallerySwiper !== null && gallerySwiper.controller &&
+        thumbnailSwiper !== null && thumbnailSwiper.controller) {
+      gallerySwiper.controller.control = thumbnailSwiper;
+      thumbnailSwiper.controller.control = gallerySwiper;
+    }
+  }, [gallerySwiper, thumbnailSwiper]);
+
+  const handleImageClick = (imageId: number) => {
+    window.document.body.style.overflow = 'hidden';
+    setIsLightboxOpen(true);
+  };
+
+  const handleCloseClick = () => {
+    window.document.body.style.overflow = 'visible';
+    setIsLightboxOpen(false);
+  };
+
+  return (<>
+    <ul className={ styles.gallery }>
+      { props.thumbnails.map((thumb: Image) =>
+        <li className={ styles.galleryItem }
+            key={ thumb.id }
+            onClick={ () => handleImageClick(thumb.id) }>
+          <img className={ styles.galleryImage }
+              src={ thumb.src }
+              alt={ thumb.alt }/>
+        </li>
+      )}
+    </ul>
+
+    <div className={ styles.lightbox + ' ' + (isLightboxOpen ? styles.open : '') }>
+      <button className={ styles.lightboxClose } onClick={ handleCloseClick } />
+
+      {/* large swiper */}
+      <div className={ styles.lightboxTop }>
+        <Swiper {...gallerySwiperParams}>
+          { props.images.map((image: Image) =>
+            <div className={ 'swiper-slide ' + styles.lightboxSlide }
+              key={ image.id }>
+              <div className='swiper-lazy-preloader' />
+              <img className={ styles.lightboxImage + ' swiper-lazy' }
+                  onLoad={(e: any) => { e.target.className += ' ' + styles.loaded; }}
+                  data-src={ image.src }
+                  alt={ image.alt }  />
+            </div>
+          )}
+        </Swiper>
+      </div>
+
+      {/* thumbnail swiper */}
+      <div className={ styles.lightboxThumbs }>
+        <Swiper {...thumbnailSwiperParams} slidesPerView='auto'>
+          { props.thumbnails.map((thumb: Image) =>
+            <div className={ styles.lightboxSlide } key={ thumb.id }>
+              <img className={ styles.lightboxImage }
+                  src={ thumb.src }
+                  alt={ thumb.alt }  />
+            </div>
+          )}
+        </Swiper>
+      </div>
+    </div>
+  </>);
+};
